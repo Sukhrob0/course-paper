cd5b5ab7f11dac72005a3d8b50561d389dc8b523
Admin(Sukhrob) <syhrob2004@gmail.com>
2023-06-27 00:14:34 +0300

    finish redaction part 1

--- php/indexregist.php
+++ php/indexregist.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/register.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
@@ -12,11 +13,8 @@
     <title>Document</title>
 </head>
 <body>
-   
+<header>
 
-
-
- <header>
         <nav class="navbar navbar-expand-lg " >
             <div class="container" style="padding-left: 250px;"   >
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
@@ -78,132 +76,43 @@
     </header>
     
 
-    <section class="vh-100" style="background-color: #eee;">
-        <div class="container h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-lg-12 col-xl-11">
-              <div class="card text-black" style="border-radius: 25px;">
-                <div class="card-body p-md-5">
-                  <div class="row justify-content-center">
-                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-      
-                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Рестрация</p>
-                      
-                
 
-    <form action="registr.php" method="post">
-    <form class="mx-1 mx-md-4">
-      
-      <div class="d-flex flex-row align-items-center mb-4">
-        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-        <div class="form-outline flex-fill mb-0">
-          <input type="text" id="form3Example1c" placeholder="Логин" name="login" class="form-control" style="border-color: #4a4d4b" />
-          <label class="form-label" for="form3Example1c">Ваш логин</label>
-        </div>
-      </div>
-
-
-      <div class="d-flex flex-row align-items-center mb-4">
-        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-        <div class="form-outline flex-fill mb-0">
-          <input type="text" id="form3Example4c" placeholder="Пароль" name="pass" class="form-control" style="border-color: #4a4d4b" />
-          <label class="form-label" for="form3Example4c">Пароль</label>
-        </div>
-      </div>
-
-      <div class="d-flex flex-row align-items-center mb-4">
-        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-        <div class="form-outline flex-fill mb-0">
-          <input type="text" id="form3Example4cd" placeholder="Повтор пороля" name="repeatpass" class="form-control" style="border-color: #4a4d4b"/>
-          <label class="form-label" for="form3Example4cd">Повторите пароль</label>
-        </div>
-      </div>
-
-      <div class="d-flex flex-row align-items-center mb-4">
-        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-        <div class="form-outline flex-fill mb-0">
-          <input type="text" id="form3Example3c" placeholder="email" name='email' class="form-control" style="border-color: #4a4d4b" />
-          <label class="form-label" for="form3Example3c">Ваш Email</label>
-        </div>
-      </div>
-
-      
-
-
-      <div class="form-check d-flex justify-content-center mb-5">
-        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" style="border-color: #4a4d4b"/>
-        <label class="form-check-label" for="form2Example3">
-          Я согласен со всеми условиями <a href="#!">Условия предостовляемых услуг</a>
-        </label>
-      </div>
-   </form>
-
-
-
-
-
-  
-    </form>
-
-
-    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <form action="registr.php" method="post">
-                          <button type="sumbit" href="registr.php" class="btn btn-primary btn-lg">Регистрация</button>
-                        </div>
-                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-      <a href="avt.php"><button type="button" class="btn btn-primary btn-lg" >У меня есть аккаунт</button> </a>
-      </div>
-    </div>
+    <h1 class='text'>Регистрация</h1>
+  <div class="wrapper">
     
-                    <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+    <form action="#">
+      <div class="input-box">
+        
+        <input type="text" placeholder="Введите логин" required>
+      </div>
+      <div class="input-box">
+        <input type="text" placeholder="Введите email" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Введите пароль" required>
+      </div>
+      <div class="input-box">
+        <input type="password" placeholder="Введите пароль повторно" required>
+      </div>
+      <div class="policy">
+        <input type="checkbox">
+        <h3>Я согласен со всеми условиями </h3>
+      </div>
+      <div class="input-box button">
+        <input type="Submit" value="Зарегестрироваться">
+      </div>
       
-                      <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                        class="img-fluid" alt="Sample image">
-      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+    </form>
+  </div>
 
 
 
 
 
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
-    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
-    integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
-    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
-    integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
-    crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.7.0.min.js"
-    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
-    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
-    integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
-    crossorigin="anonymous"></script>
-<script src="script.js"></script>
+    <script src="script.js"></script>
 
 </body>
 </html>
